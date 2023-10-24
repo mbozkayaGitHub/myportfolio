3a64a55b97991e419279759e8182a3c6140d8c9e
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-10-24 20:57:20 +0300

    Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import {Link} from "react-router-dom"
+
+
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+<div className="navbar">
+
+<Link className="link" to="/">Home</Link>
+<Link className="link" to="/projects">Projects</Link>
+<Link className="link" to="/about">About</Link>
+<Link className="link" to="/contact">Contact</Link>
+
+</div>
+
+
+
+
+
+
+    </>
   )
 }
 
