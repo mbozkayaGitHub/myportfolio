4e78ddb74ee43fa51248402c9dc2828da261b35e
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-10-24 20:57:30 +0300

    Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-
-import './App.css';
-import Home from './pages/Home/Home';
-
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Projects from "./pages/Projects/Projects";
+import Contact from "./pages/Contact/Contact";
+import Footer from "./components/Footer/Footer"
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Navbar />
+ 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+   <Footer/>
     </div>
   );
 }
