7fbda6a49b8e1da4099855448def93921a1eb41a
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-10-30 09:09:33 +0300

    Appjs

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Projects from "./pages/Projects/Projects";
 import Contact from "./pages/Contact/Contact";
-import Footer from "./components/Footer/Footer"
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <div className="App">
       <Navbar />
- 
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-   <Footer/>
+      <Footer />
     </div>
   );
 }
