9ba98568456ce254ebe50a5586a08e0d362b30f9
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-10-30 09:07:57 +0300

    Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import "./Home.scss";
+import profile from "../../assets/img/profile.avif"
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home-container'>
+<img 
+className='object_developer '
+
+
+src={profile}
+width ="500px"
+alt="developer" />
+
+<h1 className="name">Mustafa Bozkaya</h1>
+<h3>Full-Stack Developer</h3>
+
+    </div>
   )
 }
 
