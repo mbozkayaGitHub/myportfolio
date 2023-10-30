07a93410c76650b2e3567ee4ec7f61577cdad3e6
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-10-30 09:09:08 +0300

    Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,28 +1,55 @@
-import React from 'react'
-import {Link} from "react-router-dom"
-
-
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.scss";
+import Hamburger from "./Hamburger";
 
 const Navbar = () => {
+
+  const [hamburgerOpen, setHamburgerOpen] = useState(false) 
+
+  const toogleHamburger = () => {
+    setHamburgerOpen(!hamburgerOpen)
+  };
   return (
     <>
-<div className="navbar">
+      <div
+        onClick={() => {
+          toogleHamburger();
+        }}
+      >
+        <Hamburger />
+      </div>
 
-<Link className="link" to="/">Home</Link>
-<Link className="link" to="/projects">Projects</Link>
-<Link className="link" to="/about">About</Link>
-<Link className="link" to="/contact">Contact</Link>
-
-</div>
-
-
-
-
+      <div className="navbar">
+        <Link className="link" to="/">
+          Home
+        </Link>
+        <Link className="link" to="/projects">
+          Projects
+        </Link>
+        <Link className="link" to="/about">
+          About
+        </Link>
+        <Link className="link" to="/contact">
+          Contact
+        </Link>
+      </div>
+      <div className='navbar-hamburger' onMouseLeave={()=>setHamburgerOpen(false)}>
+          <Link className='link' to="/" >Home</Link>
+          <Link className='link' to="/about">About</Link>
+          <Link className='link' to="/projects">Projects</Link>
+          <Link className='link' to="/contact">Contact</Link>
+        </div>
+        <style jsx="true">
+          {`.navbar-hamburger{
+            display:${hamburgerOpen? 'flex' : 'none'}
+          }
+        `}
+        </style>
 
 
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
